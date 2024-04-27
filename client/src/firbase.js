42d48e6b2c0ab568e983be4ffcdaa5c6ead124b2
kareem <1998kareem@gmail.com>
2024-04-27 19:18:08 +0300

    added env variables

--- client/src/firbase.js
+++ client/src/firbase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import dotenv from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBBLz0n2xbM9eGUJ0HJsm1s2Cw-qOqteA8",
+    apiKey: process.env.APIKEY,
     authDomain: "twitter-clone-481bb.firebaseapp.com",
     projectId: "twitter-clone-481bb",
     storageBucket: "twitter-clone-481bb.appspot.com",
